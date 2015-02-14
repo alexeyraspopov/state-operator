4944bbf04f0e9a7f0ad654cb1ab33e645b0148c7
Avenger7x <avenger7x13@gmail.com>
2015-02-14 19:49:20 +0200

    cold observer

--- index.js
+++ index.js
@@ -4,26 +4,25 @@ var newsletter = require('newsletter'),
 function Operator(inputs, declaration){
 	var signal = newsletter(), state;
 
-	// TODO: move this part somewhere. it shouldn't work until someone will subscribe changes
-	inputs.forEach(function(tuple){
-		var key = tuple[0],
-			action = tuple[1],
-			operator = declaration[key];
-
-		action.subscribe(function(data){
-			// TODO: check operator result (maybe it's monad)
-			// state transition
-			assign(state, operator(state, data));
-			signal.publish(state);
-		});
-	});
-
 	// TODO: describe prototype with default methods implementation (initialState, etc)
 	return {
 		subscribe: function(callback){
 			if(typeof state === 'undefined'){
 				// state transition
 				state = declaration.initialState();
+
+				inputs.forEach(function(tuple){
+					var key = tuple[0],
+						action = tuple[1],
+						operator = declaration[key];
+
+					action.subscribe(function(data){
+						// TODO: check operator result (maybe it's monad)
+						// state transition
+						assign(state, operator(state, data));
+						signal.publish(state);
+					});
+				});
 			}
 
 			signal.subscribe(callback);

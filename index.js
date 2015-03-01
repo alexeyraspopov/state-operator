f2a2dd8bea3ca5c7fd2904c1a1bf9252534fd689
Avenger7x <avenger7x13@gmail.com>
2015-03-01 20:59:35 +0200

    cold -> hot observable

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 // TODO: reduce closures
 // TODO: reduce push points
 // TODO: reduce state changes (inital state and state assign)
-// TODO: move to hot observable (?)
 var newsletter = require('newsletter'),
-	assign = require('object-assign'),
-	onetime = require('onetime');
+	assign = require('object-assign');
 
 var prototype = {
 	initialState: function(){
+		// should we identify initial state?
 		return Object.create(null);
 	},
 	emit: function(state){
@@ -18,21 +17,18 @@ var prototype = {
 function Operator(inputs, declaration){
 	var signal = newsletter(),
 		methods = assign(Object.create(prototype), declaration),
-		// FIXME: hot observable!
 		state = methods.initialState();
 
-	var initialSubscribe = onetime(function(){
-		inputs.forEach(function(tuple){
-			var key = tuple[0],
-				action = tuple[1];
+	inputs.forEach(function(tuple){
+		var key = tuple[0],
+			action = tuple[1];
 
-			action.subscribe(function(data){
-				var newState = methods[key](state, data);
+		action.subscribe(function(data){
+			var newState = methods[key](state, data);
 
-				// TODO: check newState type (maybe it's monad)
-				assign(state, newState);
-				publish(signal.publish);
-			});
+			// TODO: check newState type (maybe it's monad)
+			assign(state, newState);
+			publish(signal.publish);
 		});
 	});
 
@@ -46,7 +42,6 @@ function Operator(inputs, declaration){
 
 	return {
 		subscribe: function(callback){
-			initialSubscribe();
 			initialPublish(callback);
 
 			return signal.subscribe(callback);
